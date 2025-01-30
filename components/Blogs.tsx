51252590aead3938cb97371627cb4a737f3da77b
iahmediibrahim <iahmediibrahim@gmail.com>
2025-01-30 22:48:15 +0200

    add appwrite client

---
+++ components/Blogs.tsx
@@ -0,0 +1,13 @@
+const Blogs = () => {
+	return (
+		<div className="flex flex-col gap-10">
+			<div className="flex flex-col gap-4">
+				<h1 className="text-4xl font-bold">Blogs</h1>
+				<p className="text-gray-500">
+					Here are some of the blogs I have written.
+				</p>
+			</div>
+		</div>
+	)
+}
+export default Blogs
---
+++ components/Categories.tsx
@@ -0,0 +1,13 @@
+const Categories = () => {
+	return (
+		<div className="flex flex-col gap-10">
+			<div className="flex flex-col gap-4">
+				<h1 className="text-4xl font-bold">Categories</h1>
+				<p className="text-gray-500">
+					Here are some of the categories I have written.
+				</p>
+			</div>
+		</div>
+	)
+}
+export default Categories
